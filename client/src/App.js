93353ecde7e53b35a50665fa34a51760f40c85cb
unknown <joyce.mungai@simpleformations.ke>
2022-09-15 10:22:19 +0300

    Update files and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import {Route,Routes} from 'react-router-dom'
 import './App.css';
 import Create from './components/create';
+import Edit from './components/edit';
+import Navbar from './components/navbar';
+import RecordList from './components/recordList';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<RecordList />}/>
+        <Route path="/edit/:id" element={<Edit />}/>
+        <Route path="/create" element={<Create />}/>
+      </Routes>
     <Create/>
     </div>
   );
